743b59edf0e1271100bf559cf6f772840ab85651
Alexandra Millatmal <alexandrajmh@gmail.com>
2016-05-04 11:56:35 -0500

    login with GH works

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+
+  def self.from_omniauth(auth_hash)
+    user = find_or_create_by(github_username: auth_hash['info']['nickname']) #in tutorial and previous apps there was another argument / model attribute here for provider. I removed this because my thought is that we wont need multiple providers ever. I may be thinking about this incorrectly.
+    # user.name = auth_hash['info']['name'] # don't like this because not all students will have their full name on GH and it looks like this would rewrite on every log in
+    user.image_url = auth_hash['info']['image']
+    user.save!
+    user
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   resources :users
+
+  get '/auth/:provider/callback' => 'sessions#create'
+
+  root 'users#index'
 end
