74b25dab095c7b17e2bc8be3d2e4fa5f99bde808
Alexandra Millatmal <alexandrajmh@gmail.com>
2016-05-05 15:13:35 -0500

    adds more tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -18,6 +18,25 @@ describe SessionsController, :omniauth do
     it "creates a new user" do
       expect {post :create, provider: :github}.to change{ User.count }.by(1)
     end
+
+    it "creates a session" do
+      expect(session[:user_id]).to be_nil
+      post :create, provider: :github
+      expect(session[:user_id]).not_to be_nil
+    end
+  end
+
+  describe "#destroy" do
+
+    before do
+      post :create, provider: :github
+    end
+
+    it "resets the session" do
+      expect(session[:user_id]).not_to be_nil
+      delete :destroy
+      expect(session[:user_id]).to be_nil
+    end
   end
 
 end
