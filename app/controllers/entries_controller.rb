c90745e94f409aec53dcb31a92c56b38d13f542c
Alexandra Millatmal <alexandrajmh@gmail.com>
2016-05-05 14:26:13 -0500

    puts restrictions on entry creation and access

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,5 +1,7 @@
 class EntriesController < ApplicationController
   before_action :set_entry, only: [:show, :edit, :update, :destroy]
+  before_action :user_check, only: [:new]
+  before_action :owner_check, only: [:show, :edit]
 
   # GET /entries
   def index
@@ -13,10 +15,12 @@ class EntriesController < ApplicationController
   # GET /entries/new
   def new
     @entry = Entry.new
+    @user = User.find(session[:user_id])
   end
 
   # GET /entries/1/edit
   def edit
+    @user = @entry.user_id
   end
 
   # POST /entries
@@ -49,6 +53,19 @@ class EntriesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_entry
       @entry = Entry.find(params[:id])
+    end
+
+    def user_check
+      if !current_user
+        redirect_to root_path, notice: 'You must log in first'
+      end
+    end
+
+    def owner_check
+      binding.pry
+      if current_user.id != @entry.user_id
+        redirect_to root_path, notice: 'You cannot access that page'
+      end
     end
 
     # Only allow a trusted parameter "white list" through.
