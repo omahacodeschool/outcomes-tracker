40512f269f1e328ab4e6fb3953cec35a65c87497
Alexandra Millatmal <alexandrajmh@gmail.com>
2016-05-10 11:19:46 -0500

    adds unique constrait on Profiles

---
+++ db/migrate/20160510160452_add_unique_constraint_on_profiles.rb
@@ -0,0 +1,5 @@
+class AddUniqueConstraintOnProfiles < ActiveRecord::Migration
+  def change
+    add_index :profiles, :user_id, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160506213216) do
+ActiveRecord::Schema.define(version: 20160510160452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,8 @@ ActiveRecord::Schema.define(version: 20160506213216) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", unique: true, using: :btree
 
   create_table "schools", force: :cascade do |t|
     t.string   "name"
