9e172c96e6fe3d1e9e2d177089d59e75f9afd145
Thorin Butler <thorin.butler@gmail.com>
2016-07-12 13:08:54 -0500

    ran migration creating positions table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160518174559) do
+ActiveRecord::Schema.define(version: 20160712175024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,16 @@ ActiveRecord::Schema.define(version: 20160518174559) do
 
   add_index "permissions", ["ability_id"], name: "index_permissions_on_ability_id", using: :btree
   add_index "permissions", ["user_id"], name: "index_permissions_on_user_id", using: :btree
+
+  create_table "positions", force: :cascade do |t|
+    t.string   "job_title"
+    t.string   "location"
+    t.boolean  "remote"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.integer  "user_id"
