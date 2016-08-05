ef7eb003815188aa722f3ce333b4ac693cad50dc
Thorin Butler <thorin.butler@gmail.com>
2016-08-05 11:20:23 -0500

    Add name column to positions table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160802195511) do
+ActiveRecord::Schema.define(version: 20160805161940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 20160802195511) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "entry_id"
+    t.string   "name"
   end
 
   create_table "profiles", force: :cascade do |t|
