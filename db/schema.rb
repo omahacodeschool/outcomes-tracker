52059b71a9e5b0db3c49f19a6fc0183c39037415
Alexandra Millatmal <alexandrajmh@gmail.com>
2016-08-05 09:59:40 -0500

    migration to add offer_id as a column in the salaries table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160802195511) do
+ActiveRecord::Schema.define(version: 20160805145806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -118,6 +118,7 @@ ActiveRecord::Schema.define(version: 20160802195511) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "position_id"
+    t.integer  "offer_id"
   end
 
   create_table "schools", force: :cascade do |t|
