ef594cc07710f7a760f68d67ef4c8fc4756f0fd7
Alexandra Millatmal <alexandrajmh@gmail.com>
2016-08-31 10:28:29 -0500

    addes enum to permission model

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,4 +1,10 @@
 class Permission < ActiveRecord::Base
   belongs_to :user
-  belongs_to :ability
+  # belongs_to :ability
+
+  # these abilities may change; for now, matching what abilities are in my local db / our seeds file
+  enum ability: {"can export spreadsheet" => 0, 
+                "can view all user entries" => 1, 
+                "can edit all user entries" => 2
+                }
 end
