bad260084720f8258aa25c0bd40a93aaaebc83ba
Sumeet Jain <sumeet@sumeetjain.com>
2016-09-05 21:45:35 -0500

    Fixes #102

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160905232907) do
+ActiveRecord::Schema.define(version: 20160906024251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -141,6 +141,7 @@ ActiveRecord::Schema.define(version: 20160905232907) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.text     "image_url"
+    t.datetime "last_active_at"
   end
 
   add_foreign_key "events", "entries"
