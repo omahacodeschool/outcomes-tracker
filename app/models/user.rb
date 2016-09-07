feda9d59a01d1113a628d4dbc5d3f1364f887afd
Alexandra Millatmal <alexandrajmh@gmail.com>
2016-09-07 16:41:40 -0500

    trying out this administrate plugin...

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   has_many :permissions
   has_many :events
 
+  accepts_nested_attributes_for :profile
   # accepts_nested_attributes_for :permissions
 
   has_many :job_applications, through: :entries
