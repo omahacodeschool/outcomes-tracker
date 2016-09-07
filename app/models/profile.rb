80ab5a16753c6100136e90122d41a109d5f4e34f
Sumeet Jain <sumeet@sumeetjain.com>
2016-09-07 06:42:03 -0500

    Add validations.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -6,8 +6,15 @@ class Profile < ActiveRecord::Base
 
   # Validate these on update, so that new user creation doesn't fail.
   validates :resume, presence: true, on: :update
+  validates :resume, format: { with: URI.regexp }, on: :update
+
   validates :linked_in, presence: true, on: :update
+  validates :linked_in, format: { with: URI.regexp }, on: :update
+
   validates :personal_website, presence: true, on: :update
+  validates :personal_website, format: { with: URI.regexp }, on: :update
+
+  validates :twitter, format: { with: /\A(-|\w)*\z/i }, on: :update
 
   def empty?
     gender.blank?
