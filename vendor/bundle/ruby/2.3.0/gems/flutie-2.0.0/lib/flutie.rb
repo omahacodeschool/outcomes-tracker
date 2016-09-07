1c4adfc392464b626ccb08ff58a0e007b0ce2aa3
Sumeet Jain <sumeet@sumeetjain.com>
2016-09-06 19:56:31 -0500

    Remove vender/bundle to silence Heroku warning

--- vendor/bundle/ruby/2.3.0/gems/flutie-2.0.0/lib/flutie.rb
+++
@@ -1,5 +0,0 @@
-require 'flutie/version'
-
-module Flutie
-  require 'flutie/railtie' if defined?(Rails)
-end
--- vendor/bundle/ruby/2.3.0/gems/honeybadger-2.6.0/lib/honeybadger/version.rb
+++
@@ -1,4 +0,0 @@
-module Honeybadger
-  # Public: The current String Honeybadger version.
-  VERSION = '2.6.0'.freeze
-end
