81a1e239c0edff1da3ee62bf4a0fc57b0c787593
Sumeet Jain <sumeet@sumeetjain.com>
2016-09-07 18:38:07 -0500

    Remove entries.company column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160907232346) do
+ActiveRecord::Schema.define(version: 20160907233740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,7 +51,6 @@ ActiveRecord::Schema.define(version: 20160907232346) do
 
   create_table "entries", force: :cascade do |t|
     t.integer  "user_id"
-    t.string   "company"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "company_id"
