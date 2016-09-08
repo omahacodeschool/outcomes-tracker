9c7e4119564cf63643534ff5844732b4e35ad73a
Alexandra Millatmal <alexandrajmh@gmail.com>
2016-09-08 16:43:48 -0500

    process working when a user advances their job app up to an offer. might be some duplication here...not really sure what Sumeet envisioned

--- app/mailers/new_event_mailer.rb
+++ app/mailers/new_event_mailer.rb
@@ -10,4 +10,14 @@ class NewEventMailer < ApplicationMailer
 
     mail(to: emails, subject: subject)
   end
+
+  def new_offer_event_email(users, user, entry)
+    @users = users
+    @entry = entry
+
+    emails = @users.map(&:email)
+    subject = "#{user.name} just received an offer from #{@entry.company.name}!"
+
+    mail(to: emails, subject: subject)
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -6,9 +6,21 @@ class Event < ActiveRecord::Base
 
   validates :notes, presence: true
 
+
+  def self.for_offer(offer)
+    event = Event.create({entry_id: offer.entry_id, user_id: offer.user.id, notes:"I recieved an offer!"})
+    if event.save
+      event.notify_offer
+    end
+  end
+
   # Send email to everyone involved in the conversation.
   def notify
     NewEventMailer.new_event_email(users_in_conversation, entry).deliver_now
+  end
+
+  def notify_offer
+    NewEventMailer.new_offer_event_email(users_in_conversation, user, entry).deliver_now
   end
 
   # For a event/comment, checks what other users have events/comments in the same thread or who should be notified of the event/comment.
