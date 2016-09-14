b28582fac59ebab05c0baadaadacb7f8c4a0b8b8
Sumeet Jain <sumeet@sumeetjain.com>
2016-09-14 17:01:34 -0500

    Basic views for Companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,7 +1,11 @@
 class CompaniesController < ApplicationController
+  before_action :admin_only
+
   def index
+    @companies = Company.permitted_for(current_user).includes(entries: :job_application)
   end
 
   def show
+    @company = Company.includes(entries: :job_application).find(params[:id])
   end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -6,4 +6,10 @@ class Company < ActiveRecord::Base
   def hidden_for_user?(user)
     user.hidings.where(company_id: id).any?
   end
+
+  scope :permitted_for, -> (viewing_user) do
+    joins("LEFT OUTER JOIN hidings ON companies.id = hidings.company_id")
+    .select("companies.id, companies.name, hidings.user_id")
+    .where("hidings.user_id IS NULL OR hidings.user_id != #{viewing_user.id}")
+  end
 end
