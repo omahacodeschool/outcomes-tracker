c4b41346f29c630d2ae2d51d4d44d78c18ab25d3
Alexandra Millatmal <alexandrajmh@gmail.com>
2016-10-27 15:58:52 -0500

    moves location of mail helper and changes config file

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module OutcomesTracker
     config.action_controller.action_on_unpermitted_parameters = :raise
     config.active_record.raise_in_transactional_callbacks = true
     config.active_job.queue_adapter = :delayed_job
-    config.autoload_paths << "#{Rails.root}/lib"
+    config.autoload_paths << "#{Rails.root}/app/mailers/helpers"
   end
 end
