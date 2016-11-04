f043ecf1a0484ae9e6c5ebe3f74f67038b2f9c5d
Alexandra Millatmal <alexandrajmh@gmail.com>
2016-11-04 16:53:17 -0500

    writes functionality to find upcoming cohorts and the next occuring cohort

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,4 +1,13 @@
 class Cohort < ActiveRecord::Base
   has_many :profiles
   has_many :users, through: :profiles
+
+  def self.next
+  	(upcoming.length > 0) ? upcoming[0].id : nil
+  end
+
+  def self.upcoming
+  	Cohort.where("start_date > ?", Date.today).order(start_date: :asc)
+  end
+
 end
