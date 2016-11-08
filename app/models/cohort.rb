5ff88554e378670fb49e632233395f0d33f8fd79
Alexandra Millatmal <alexandrajmh@gmail.com>
2016-11-08 15:18:48 -0600

    changes upcoming method to a scope in the Cohort model

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -2,12 +2,10 @@ class Cohort < ActiveRecord::Base
   has_many :profiles
   has_many :users, through: :profiles
 
+  scope :upcoming, -> {where("start_date > ?", Date.today).order(start_date: :asc)}
+
   def self.next
   	(upcoming.length > 0) ? upcoming[0] : nil
-  end
-
-  def self.upcoming
-  	Cohort.where("start_date > ?", Date.today).order(start_date: :asc)
   end
 
 end
