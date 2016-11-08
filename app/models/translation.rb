05bfe79776474fca38dc83743af9b8cd586cba1f
Alexandra Millatmal <alexandrajmh@gmail.com>
2016-11-08 15:54:59 -0600

    changes methods to scopes and deletes extra FA script

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -3,21 +3,24 @@ class Translation < ActiveRecord::Base
 	belongs_to :project
 	attr_accessor :project_name
 
+	scope :finished, -> {where("output_text IS NOT NULL").order("updated_at DESC")}
+	scope :unfinished, -> {where("output_text IS NULL").order("created_at DESC")}
+
 	def truncate
 		self.input_text[0,50] + " ..."
 	end
 
 	def self.unfinished?
-		return true if self.all_unfinished.count > 0
+		self.unfinished.count > 0
 	end
 
-	def self.finished
-		self.where("output_text IS NOT NULL").order("updated_at DESC")
-	end
+	# def self.finished
+	# 	self.where("output_text IS NOT NULL").order("updated_at DESC")
+	# end
 
-	def self.unfinished
-		self.where("output_text IS NULL").order("created_at DESC")
-	end
+	# def self.unfinished
+	# 	self.where("output_text IS NULL").order("created_at DESC")
+	# end
 
 	def icon
 		if self.output_text
