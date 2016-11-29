fdd3acca4e19caa6cc491e6058efe31cb19d3e13
Alexandra Millatmal <alexandrajmh@gmail.com>
2016-11-29 16:01:56 -0600

    finishes decorated position tests

--- spec/decorators/decorated_position_spec.rb
+++ spec/decorators/decorated_position_spec.rb
@@ -2,46 +2,10 @@ require 'rails_helper'
 
 describe DecoratedPosition do
 
-  # before :each do
-  #   @entry = double()
-  #   @decorated_entry = DecoratedEntry.new(@entry)
-  # end
-
   before :each do
     @position = double()
     @decorated_position = DecoratedPosition.new(@position)
   end
-
-  # it 'returns nil for a non-existent offer' do
-  #   @offer = double()
-  # allow(@entry).to receive(:offer) { nil }
-  #   expect(@decorated_entry.offer).to eq(nil)
-  # end
-
-  # it 'returns offer object for an existing offer' do
-  #   @offer = double()
-  #   allow(@entry).to receive(:offer) { @offer }
-  #   expect(@decorated_entry.offer).to be_a(DecoratedOffer)
-  # end
-
-  # it 'returns nil for a non-existent position' do
-  #   @position = double()
-  #   allow(@entry).to receive(:position) { nil }
-  #   expect(@decorated_entry.position).to eq(nil)
-  # end
-
-  # it 'returns position object for an existing position' do
-  #   @position = double()
-  #   allow(@entry).to receive(:position) { @position }
-  #   expect(@decorated_entry.position).to be_a(DecoratedPosition)
-  # end
-
-  # it 'returns a collection of associated events as decorated events' do
-  #   @event1 = double()
-  #   @event2 = double()
-  #   allow(@entry).to receive(:events_history) { [@event1, @event2] }
-  #   expect(@decorated_entry.events_history[0]).to be_a(DecoratedEvent)
-  # end
 
   it 'formats position salary amount' do
     allow(@position).to receive_message_chain(:salary, :amount) { 25 }
@@ -54,8 +18,14 @@ describe DecoratedPosition do
     expect(@decorated_position.formatted_acceptance_time).to eq('less than a minute')
   end
 
-  # it 'returns correct remote message' do
-  # 	#
-  # end
+  it 'returns nil for remote message when position not remote' do
+  	allow(@position).to receive(:remote) { false }
+    expect(@decorated_position.remote_message).to be_nil
+  end
+
+  it 'returns remote message when position is remote' do
+    allow(@position).to receive(:remote) { true }
+    expect(@decorated_position.remote_message).to eq('This is a remote position.')
+  end
 
 end
